d1690df468b00adf3ed5e2967fd4893f550828d2
jwl317 <--unset-all>
2019-11-14 19:18:09 +0900

    Add file

---
+++ report_card.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+	printf("This is Report Cards.c\n");
+	printf("what is the difference ? \n");
+	printf("Math: 180 \n");
+	printf("English: 100 \n");
+	printf("Sum grade: 190 \n");
+	printf("Average grade: 95 \n");
+}
+
